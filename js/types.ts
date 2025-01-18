287cc6aaf640b280c66d7ea9473404fc1accb79a
David Kramer <davidtkramer@icloud.com>
2025-01-18 10:52:16 -0800

    wip

--- js/types.ts
+++ js/types.ts
@@ -2,3 +2,18 @@ export type Choices = Record<number, string>;
 export type SignalValue = number | string;
 export type ByteOrder = "little_endian" | "big_endian";
 export type Comments = Record<string, string>;
+
+export type Node = {
+  id?: string;
+  name?: string;
+  comment?: string;
+  comments?: Comments;
+}
+
+export type Bus = {
+  name: string;
+  comment?: string;
+  comments?: Comments;
+  baudrate?: number;
+  fdBaudrate?: number;
+}
